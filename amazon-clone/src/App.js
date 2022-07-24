1ef58ca2cb9ebb781d0276e735523f2abb247357
KANATA <sanjana2842002@gmail.com>
2022-07-24 15:38:23 +0530

    .

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
@@ -44,6 +43,7 @@ export default function App() {
               <>
                 <Header />
                 <Checkout />
+                <Footer />
               </>
             }
           />
@@ -54,11 +54,11 @@ export default function App() {
               <>
                 <Header />
                 <Home />
+                <Footer />
               </>
             }
           />
         </Routes>
-        <Footer />
       </div>
     </Router>
   );
